f28e2c88dd6e851dd2a3c685ecdd3a51daa8060e
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-04-17 01:26:18 +0600

    Added .gitignore file

--- Advanced/Linq/obj/Debug/net8.0/Linq.AssemblyInfo.cs
+++ Advanced/Linq/obj/Debug/net8.0/Linq.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Linq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89938fa48feffb4d8dc81fca4afa576cde02c548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2cfa5e07a86cd0ece9560aa83eb3c57e5a53a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Linq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Linq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OOP/Namespaces/obj/Debug/net8.0/Namespaces.AssemblyInfo.cs
+++ OOP/Namespaces/obj/Debug/net8.0/Namespaces.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Namespaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2cfa5e07a86cd0ece9560aa83eb3c57e5a53a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Namespaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Namespaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
